4be375c22354d16a076b407c0baeba1a65703ea7
Dayna Lowe <dayna.c.lowe@gmail.com>
2015-02-15 10:03:18 -0500

    Added devise, changed routes, added styled nav bar and edited views.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,10 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+$(document).ready(function () {
+        $(".navbar-toggle").on("click", function () {
+            $(this).toggleClass("active");
+        });
+    });
