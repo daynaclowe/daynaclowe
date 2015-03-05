e9b30df1295cd1e64cdf74765d12e1afde504189
Dayna Lowe <dayna.c.lowe@gmail.com>
2015-03-05 18:29:28 -0500

    Working on responsiveness

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,3 +22,13 @@ $(document).ready(function () {
             $(this).toggleClass("active");
         });
     });
+
+$(function(){
+  var $ppc = $('.progress-pie-chart'),
+    percent = parseInt($ppc.data('percent')),
+    deg = 360*percent/100;
+  if (percent > 50) {
+    $ppc.addClass('gt-50');
+  }
+  $('.ppc-progress-fill').css('transform','rotate('+ deg +'deg)');
+});
